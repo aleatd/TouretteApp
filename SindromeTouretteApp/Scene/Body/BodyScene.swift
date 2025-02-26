b57272777711ff7c3c60b2a9e51796d11146c7cf
Benny-001 <131472044+Benny-001@users.noreply.github.com>
2025-02-26 10:31:46 +0100

    v. 1.1

--- SindromeTouretteApp/Scene/Body/BodyScene.swift
+++ SindromeTouretteApp/Scene/Body/BodyScene.swift
@@ -7,8 +7,8 @@ class BodyScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-       
-      
+        
+        
         background.size.height = 690.0
         background.position = CGPoint(x: size.width / 2, y: size.height / 2)
         addChild(background)
@@ -18,13 +18,13 @@ class BodyScene: SKScene {
             
             ("Left Shoulder", CGPoint(x: size.width / 2 - 75, y: size.height / 2 + 90), 36),
             ("Right Shoulder", CGPoint(x: size.width / 2 + 75, y: size.height / 2 + 90), 36),
-
+            
             ("Left Arm", CGPoint(x: size.width / 2 - 100, y: size.height / 2 - 10), 56),
             ("Right Arm", CGPoint(x: size.width / 2 + 100, y: size.height / 2 - 10), 56),
             
             ("Left Hand", CGPoint(x: size.width / 2 - 125, y: size.height / 2 - 115), 36),
             ("Right Hand", CGPoint(x: size.width / 2 + 125, y: size.height / 2 - 115), 36),
-
+            
             ("Left Leg", CGPoint(x: size.width / 2 - 50, y: size.height / 4), 56),
             ("Right Leg", CGPoint(x: size.width / 2 + 50, y: size.height / 4), 56),
             
@@ -68,30 +68,33 @@ class BodyScene: SKScene {
         
         let node = self.atPoint(location)
         if let name = node.name {
-<<<<<<< Updated upstream
+            
             print("\(name)")
             if let dot = node.childNode(withName: "dot") as? Dot {
                 dot.activate()
-=======
-            partName?.wrappedValue = name
-            
-            if name == "Head" {
-                zoomIn(background,CGPoint(x: node.position.x - 190 , y: node.position.y - 150) )
->>>>>>> Stashed changes
+                
+                partName?.wrappedValue = name
+                
+                if name == "Head" {
+                    zoomIn(background,CGPoint(x: node.position.x - 190 , y: node.position.y - 150) )
+                    
+                }
             }
         }
     }
-    
-    func zoomIn(_ node :SKSpriteNode, _ targetCoor : CGPoint) {
-        let targetPosition = node.convert(targetCoor, to: self)
         
-        let dx = (node.position.x - targetPosition.x) * (1.5 - 1)
-        let dy = (node.position.y - targetPosition.y) * (1.5 - 1)
-        
-        let moveAction  = SKAction.move(by: CGVector(dx: dx, dy: dy), duration: 0.5)
-        let scaleAction = SKAction.scale(by: 1.8, duration: 0.5)
-        
-        let group = SKAction.group([moveAction,scaleAction])
-        node.run(group)
+        func zoomIn(_ node :SKSpriteNode, _ targetCoor : CGPoint) {
+            let targetPosition = node.convert(targetCoor, to: self)
+            
+            let dx = (node.position.x - targetPosition.x) * (1.5 - 1)
+            let dy = (node.position.y - targetPosition.y) * (1.5 - 1)
+            
+            let moveAction  = SKAction.move(by: CGVector(dx: dx, dy: dy), duration: 0.5)
+            let scaleAction = SKAction.scale(by: 1.8, duration: 0.5)
+            
+            let group = SKAction.group([moveAction,scaleAction])
+            node.run(group)
+        }
     }
-}
+
+        
