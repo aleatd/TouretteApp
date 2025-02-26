00a4626ad4086377937a485ef624ec3faf068e02
Benny-001 <131472044+Benny-001@users.noreply.github.com>
2025-02-26 11:26:40 +0100

    1.2.2

--- SindromeTouretteApp/ContentView.swift
+++ SindromeTouretteApp/ContentView.swift
@@ -2,25 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selected = 0
-    
-    init() {
-        let appeareance = UITabBarAppearance()
-        appeareance.backgroundColor = .brown
-        appeareance.shadowColor = .white
         
-        appeareance.stackedLayoutAppearance.normal.iconColor = .white
-        appeareance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        appeareance.stackedLayoutAppearance.selected.iconColor = .black
-        appeareance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.black]
-        
-        UITabBar.appearance().standardAppearance = appeareance
-        UITabBar.appearance().scrollEdgeAppearance = appeareance
-    }
-    
     var body: some View {
-        
-        TabView(selection: $selected) {
+        /*
+         TabView(selection: $selected) {
             DataView()
                 .tabItem {
                     Image(systemName: "text.page")
@@ -43,7 +28,10 @@ struct ContentView: View {
         .background(
             Rectangle().stroke(Color.black, lineWidth: 5)
         )
-      
+        */
+        
+        
+        
     }
 }
 
